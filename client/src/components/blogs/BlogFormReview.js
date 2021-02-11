8a80fc7633468902e7520b0c2d8c84326581dcbc
viettran1010 <viettran10101988@gmail.com>
2021-02-11 12:54:09 +0700

    add jest config to solve mongoose problem

--- client/src/components/blogs/BlogFormReview.js
+++ client/src/components/blogs/BlogFormReview.js
@@ -7,6 +7,8 @@ import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
 class BlogFormReview extends Component {
+  state = {file: null}
+
   renderFields() {
     const { formValues } = this.props;
 
@@ -44,7 +46,11 @@ class BlogFormReview extends Component {
 
     const { submitBlog, history, formValues } = this.props;
 
-    submitBlog(formValues, history);
+    submitBlog(formValues, this.state.file, history);
+  }
+
+  onFileChanged(event) {
+    this.setState({file: event.target.files[0]})
   }
 
   render() {
@@ -52,6 +58,9 @@ class BlogFormReview extends Component {
       <form onSubmit={this.onSubmit.bind(this)}>
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
+
+        <h5>Add An Image</h5>
+        <input type="file" accept="image/*" onChange={this.onFileChanged.bind(this)}/>
 
         {this.renderButtons()}
       </form>
