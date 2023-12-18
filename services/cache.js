3080ca0f4e0d9cf7d9c9fc42d13426651c020cd4
Nanete (Jitera) <developers@jitera.com>
2023-12-18 07:58:59 +0000

    feat[business-logic]: implement 'Invalidate cache entry' Changes * '/services/cache.js': + To address the requirement of invalidating a cache entry, you need to implement a function within the `/services/cache.js` file that handles the invalidation process..+ The function should be named `invalidateCacheEntry` and accept a single parameter `qu...

--- services/cache.js
+++ services/cache.js
@@ -6,6 +6,8 @@ const keys = require('../config/keys');
 const client = redis.createClient(keys.redisUrl);
 client.hget = util.promisify(client.hget);
 client.hset = util.promisify(client.hset); // Promisify hset for async/await usage
+client.hexists = util.promisify(client.hexists); // Promisify the hexists function from new code
+client.hdel = util.promisify(client.hdel); // Promisify the hdel function from new code
 const exec = mongoose.Query.prototype.exec; // reference to the original exec function
 
 mongoose.Query.prototype.cache = function(options = {}) {
@@ -13,7 +15,7 @@ mongoose.Query.prototype.cache = function(options = {}) {
     this.hashKey = JSON.stringify(options.key || '');
 
     return this; // TO MAKE THIS CHAINABLE
-}
+};
 
 mongoose.Query.prototype.exec = async function() { // override the exec function
     if (!this.useCache) {
@@ -22,30 +24,56 @@ mongoose.Query.prototype.exec = async function() { // override the exec function
 
     const key = JSON.stringify({...this.getQuery(), collection: this.mongooseCollection.name});
 
-    // Check for an existing entry in Redis using the "query_key"
+    // See if we have a value for 'key' in redis
     const cacheValue = await client.hget(this.hashKey, key);
 
-    // If an existing entry is found, return that
+    // If we do, return that
     if (cacheValue) {
         const doc = JSON.parse(cacheValue);
+
         return Array.isArray(doc) 
-            ? doc.map(d => new this.model(d))
-            : new this.model(doc);
+        ? doc.map(d => new this.model(d))
+        : new this.model(doc);
     }
 
-    // Otherwise, issue the query and store the result in Redis
+    // Otherwise, issue the query and store the result in redis
     const result = await exec.apply(this, arguments);
-
-    // Serialize the "query_result" using JSON.stringify before storing it in Redis
     await client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10).catch(err => {
         console.error('Redis hset failed:', err);
     });
-
+    
     return result;
+};
+
+/**
+ * Invalidates a cache entry identified by the query_key.
+ * @param {string} query_key - The key of the query to invalidate.
+ * @returns {Promise<string>} - A message indicating the outcome.
+ */
+async function invalidateCacheEntry(query_key) {
+    try {
+        const hashKey = JSON.stringify(query_key.hashKey);
+        const key = JSON.stringify(query_key.key);
+
+        // Check if the entry exists in the cache
+        const exists = await client.hexists(hashKey, key);
+        if (exists) {
+            // If it exists, delete the entry
+            await client.hdel(hashKey, key);
+            return 'Cache entry has been successfully invalidated.';
+        } else {
+            // If it doesn't exist, return a message indicating nothing to invalidate
+            return 'No matching cache entry found to invalidate.';
+        }
+    } catch (error) {
+        console.error('Error invalidating cache entry:', error);
+        throw new Error('Failed to invalidate cache entry.');
+    }
 }
 
 module.exports = {
     clearHash(hashKey) {
         client.del(JSON.stringify(hashKey));
-    }
-}
+    },
+    invalidateCacheEntry // Export the new invalidateCacheEntry function
+};
