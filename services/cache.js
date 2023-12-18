def48526c3d1a90e4b017bad9ea5d3cb19917723
Celestia (Jitera) <developers@jitera.com>
2023-12-18 07:56:30 +0000

    feat[business-logic]: implement 'Cache query results' Changes * '/services/cache.js': + Update the `cache` function in the `mongoose.Query.prototype` to accept a "query_key" as an argument and use it as the `hashKey` for caching..+ Modify the `exec` function to serialize the "query_result" using `JSON.stringify` before storing it in Redis w...

--- services/cache.js
+++ services/cache.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const redis = require('redis');
 const util = require('util');
-const keys = require('../config/keys')
+const keys = require('../config/keys');
 
-// const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(keys.redisUrl);
 client.hget = util.promisify(client.hget);
+client.hset = util.promisify(client.hset); // Promisify hset for async/await usage
 const exec = mongoose.Query.prototype.exec; // reference to the original exec function
 
 mongoose.Query.prototype.cache = function(options = {}) {
@@ -16,32 +16,31 @@ mongoose.Query.prototype.cache = function(options = {}) {
 }
 
 mongoose.Query.prototype.exec = async function() { // override the exec function
-    if (this.useCache === false) {
-        return exec.apply(this, arguments)
+    if (!this.useCache) {
+        return exec.apply(this, arguments);
     }
 
-    // console.log('IM ABOUT TO RUN A QUERY');
-    const key = JSON.stringify({...this.getQuery(), collection: this.mongooseCollection.name})
-    // console.log(key)
+    const key = JSON.stringify({...this.getQuery(), collection: this.mongooseCollection.name});
 
-    // See if we have a value for 'key' in redis
+    // Check for an existing entry in Redis using the "query_key"
     const cacheValue = await client.hget(this.hashKey, key);
 
-    // If we do, return that
+    // If an existing entry is found, return that
     if (cacheValue) {
-        console.log('cacheValue: ',cacheValue);
-        const doc = JSON.parse(cacheValue)
-
+        const doc = JSON.parse(cacheValue);
         return Array.isArray(doc) 
-        ? doc.map(d=>new this.model(d))
-        : new this.model(doc);
+            ? doc.map(d => new this.model(d))
+            : new this.model(doc);
     }
 
-    // Otherwise, issue the query and store the result in reids
-
+    // Otherwise, issue the query and store the result in Redis
     const result = await exec.apply(this, arguments);
-    client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10)
-    
+
+    // Serialize the "query_result" using JSON.stringify before storing it in Redis
+    await client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10).catch(err => {
+        console.error('Redis hset failed:', err);
+    });
+
     return result;
 }
 
