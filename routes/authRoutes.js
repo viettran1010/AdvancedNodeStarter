f6da84035ce9373bd26937ae088db417bec0c481
Terza (Jitera) <developers@jitera.com>
2023-12-18 07:46:55 +0000

    feat[business-logic]: implement 'Create user account and request' Changes * '/models/Request.js': + Create a new Mongoose schema named Request with fields for area, menuSelection, additionalDetails, and a reference to the User schema to link the request to the user..+ Ensure that the schema is properly exported and can be used in other parts of the app... * '/services/userService.js': + Create a new service file named userService.js to handle business logic related to user accounts..+ Write a function 'createUserAccount' that takes the user's input data, validates it, checks for an existing account with the same email, and creates a new... * '/services/requestService.js': + Create a new service file named requestService.js to encapsulate the business logic for handling user requests..+ Write a function 'createUserRequest' that takes the request-related input data and creates a new record in the "requests" table..+ This func... * '/models/User.js': + Update the User schema to include additional fields: email, password, displayName, birthDate, and gender. Ensure that the email field is unique to prevent duplicate accounts..+ Implement a method to encrypt the password before saving it to the database. ... * '/routes/authRoutes.js': To implement the user account creation and request flow, you will need to add new endpoints to handle user registration and email verification. Here's how you can modify the `authRoutes.js` file to include these functionalities:..1. Add a new endpoint for ... * '/services/photoService.js': + Create a new service file named photoService.js to manage the logic for handling photo uploads linked to user requests..+ Write a function 'storeUploadedPhotos' that takes an array of photo file information, creates new records in the "photos" table, and... * '/models/Photo.js': + Define a new Mongoose schema named Photo with fields for filePath, fileSize, and a reference to the Request schema to associate the photo with a request..+ Ensure that the schema includes any necessary validation for the file path and file size.... * '/routes/uploadRoutes.js': + Modify the existing uploadRoutes.js file to handle the photo upload process as part of the user request creation flow..+ Ensure that the route utilizes the 'storeUploadedPhotos' function from photoService.js to save the photo information to the database....

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,14 @@
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
-const { Op } = require('sequelize'); // Import Sequelize operator
 const passport = require('passport'); // Assuming passport is required for authentication
-const { sendPasswordResetEmail } = require('../services/mailingService'); // Assuming there's a mailing service
+const { Op } = require('sequelize'); // Import Sequelize operator
 
 // Assuming models are exported from a central file
-const { User, PasswordResetLink } = require('../models');
+const { User, PasswordResetLink, Request, Photo } = require('../models');
+
+// Assuming the emailService and mailingService are two different services that need to be combined
+const { sendVerificationEmail } = require('../services/emailService');
+const { sendPasswordResetEmail } = require('../services/mailingService');
 
 module.exports = app => {
   app.get(
@@ -104,10 +107,69 @@ module.exports = app => {
     }
   });
 
-  // New route handler for email verification
-  app.post('/api/verify_account', async (req, res) => {
+  // New route handler for user registration
+  app.post('/api/register', async (req, res) => {
     try {
-      const { token } = req.body;
+      const { email, password, displayName, birthDate, gender, area, menuSelection, additionalDetails, filePath, fileSize } = req.body;
+
+      // Validate input fields (implement validation logic or use a library like express-validator)
+      if (!email || !password || !displayName || !birthDate || !gender || !area || !menuSelection || !filePath || !fileSize) {
+        return res.status(400).send({ error: 'All fields are required' });
+      }
+
+      // Additional validation can be added here (e.g., email format, password policy, etc.)
+
+      // Check if the email is already registered
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).send({ error: 'Email already in use' });
+      }
+
+      // Encrypt the password before saving
+      const salt = await bcrypt.genSalt(10);
+      const encryptedPassword = await bcrypt.hash(password, salt);
+
+      // Create a new user with encrypted password and other details
+      const user = new User({ email, password: encryptedPassword, displayName, birthDate, gender, accountVerified: false });
+      await user.save();
+
+      // Create a new request record linked to the user
+      const request = new Request({ area, menuSelection, additionalDetails, status: 'pending', user_id: user._id });
+      await request.save();
+
+      // Create a new photo record linked to the request
+      const photo = new Photo({ filePath, fileSize, request_id: request._id });
+      await photo.save();
+
+      // Generate email verification token
+      const verificationToken = crypto.randomBytes(20).toString('hex');
+      const expiresAt = new Date();
+      expiresAt.setHours(expiresAt.getHours() + 24); // Token expires in 24 hours
+
+      // Save the verification token
+      const passwordResetLink = new PasswordResetLink({
+        token: verificationToken,
+        expires_at: expiresAt,
+        used: false,
+        user_id: user._id
+      });
+      await passwordResetLink.save();
+
+      // Send verification email
+      await sendVerificationEmail(user.email, verificationToken);
+
+      // Return success message
+      res.send({ message: 'Account and request created. Verification email sent.' });
+    } catch (error) {
+      console.error('Error during user registration:', error);
+      res.status(500).send({ error: 'Internal server error' });
+    }
+  });
+
+  // New route handler for email verification
+  app.get('/api/verify_email/:token', async (req, res) => {
+    try {
+      const { token } = req.params;
       if (!token) {
         return res.status(400).send({ error: 'Token is required' });
       }
--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,23 +1,82 @@
-const AWS = require('aws-sdk')
-const keys = require('../config/keys')
-const requireLogin = require('../middlewares/requireLogin')
-const uuid = require('uuid/v1')
+const AWS = require('aws-sdk');
+const keys = require('../config/keys');
+const requireLogin = require('../middlewares/requireLogin');
+const uuid = require('uuid/v1');
+const userService = require('../services/userService'); // Assuming userService.js exists and handles user-related operations
+const requestService = require('../services/requestService'); // Assuming requestService.js exists and handles request-related operations
+const photoService = require('../services/photoService'); // Assuming photoService.js exists and handles photo-related operations
+const emailService = require('../services/emailService'); // Assuming emailService.js exists and handles email-related operations
 
 const s3 = new AWS.S3({
     signatureVersion: 'v4',
     region: 'ap-southeast-1',
     accessKeyId: keys.accessKeyId,
     secretAccessKey: keys.secretAccessKey
-})
+});
 
 module.exports = app => {
-    app.get('/api/upload', requireLogin, (req,res)=> {
+    app.get('/api/upload', requireLogin, (req, res) => {
         const key = `${req.user.id}/${uuid()}.jpeg`;
 
         s3.getSignedUrl('putObject', {
             Bucket: 'viet-blogs',
             ContentType: 'image/jpeg',
             Key: key
-        }, (err, url)=> res.send({key, url}))
-    })    
+        }, (err, url) => res.send({ key, url }));
+    });
+
+    app.post('/api/upload/photo', requireLogin, async (req, res) => {
+        const { request_id } = req.body;
+        const files = req.files;
+
+        if (!files || files.length === 0) {
+            return res.status(400).send({ error: 'No files uploaded.' });
+        }
+
+        try {
+            const photos = await photoService.storeUploadedPhotos(files, request_id);
+            res.status(201).send(photos);
+        } catch (error) {
+            res.status(500).send({ error: 'Error saving photo information to the database.' });
+        }
+    });
+
+    // New route to handle user account and request creation
+    app.post('/api/users/create', async (req, res) => {
+        const { email, password, display_name, birth_date, gender, area, menu_selection, additional_details, files } = req.body;
+
+        // Validate input fields (pseudo-code, assuming validation functions exist)
+        if (!userService.validateEmail(email) || !userService.validatePassword(password) || !userService.validateDisplayName(display_name) || !userService.validateBirthDate(birth_date) || !userService.validateGender(gender)) {
+            return res.status(400).send({ error: 'Invalid input data.' });
+        }
+
+        try {
+            // Check if the email is already registered
+            if (await userService.isEmailRegistered(email)) {
+                return res.status(400).send({ error: 'Email is already registered.' });
+            }
+
+            // Create user account
+            const user = await userService.createUser(email, password, display_name, birth_date, gender);
+
+            // Create request
+            const request = await requestService.createRequest(user.id, area, menu_selection, additional_details);
+
+            // Store uploaded photos
+            const photos = await photoService.storeUploadedPhotos(files, request.id);
+
+            // Generate email verification token and send email
+            const verificationToken = await userService.generateEmailVerificationToken(user.id);
+            await emailService.sendVerificationEmail(email, verificationToken);
+
+            res.status(201).send({
+                message: 'Account and request created successfully. Verification email sent.',
+                user: user,
+                request: request,
+                photos: photos
+            });
+        } catch (error) {
+            res.status(500).send({ error: 'Error creating account and request.' });
+        }
+    });
 }
