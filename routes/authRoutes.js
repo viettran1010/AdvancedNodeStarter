a49aa1c8442cf4328384bc588a3b156261ed1bfc
Carline (Jitera) <developers@jitera.com>
2023-12-18 08:00:26 +0000

    feat[business-logic]: implement 'Register user' Changes * '/models/User.js': To implement the registration logic, you will need to update the User model to include fields for `email` and `password`. You will also need to add methods for password hashing and verification. Here's how you can update the User model:..1. Add `email` and... * '/routes/authRoutes.js': This file will handle the registration endpoint and include the business logic for registering a user. To implement the registration flow, you will need to create a new route that listens for POST requests on the path designated for user registration (e.g....

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
-const bcrypt = require('bcryptjs'); // Assuming bcryptjs is a common module for hashing
-const { User } = require('../models/User'); // Assuming User model exists and is exported from /models/User
+const bcrypt = require('bcryptjs'); // Use bcryptjs as it is the common module for hashing in the existing code
+const { body, validationResult } = require('express-validator');
+const { User } = require('../models/User'); // Use destructuring as it is used in the existing code
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
 const { clearHash } = require('../services/cacheService'); // Assuming cacheService exists and provides clearHash function
 
 module.exports = app => {
@@ -26,6 +29,55 @@ module.exports = app => {
 
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
+  });
+
+  // New registration route
+  app.post('/api/register', [
+    body('name').not().isEmpty().withMessage('Name is required'),
+    body('email').isEmail().withMessage('Invalid email format'),
+    body('password').not().isEmpty().withMessage('Password is required'),
+    body('password_confirmation').custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Password confirmation does not match password');
+      }
+      return true;
+    }),
+  ], async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const { name, email, password } = req.body;
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'Email is already in use' });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+        // Assuming there is a field to indicate email verification status
+        emailVerified: false
+      });
+
+      const confirmationToken = jwt.sign({ id: newUser.id }, 'secret', { expiresIn: '1d' }); // Replace 'secret' with your secret key
+
+      // Send confirmation email (nodemailer setup and sending logic goes here)
+
+      res.status(200).json({
+        id: newUser.id,
+        name: newUser.name,
+        email: newUser.email,
+        message: 'A confirmation email has been sent. Please check your inbox.'
+      });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   });
 
   // New route handler for updating user profile
