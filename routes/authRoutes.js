d487b1431e522d4a3ab34f1ebdb067a59ff9bf9c
Sabina (Jitera) <developers@jitera.com>
2023-12-18 07:39:06 +0000

    feat[business-logic]: implement 'Verify user account' Changes * '/routes/authRoutes.js': + Create a new route handler to receive the token from the user's email verification link. This route could be something like `app.post('/api/verify_account', async (req, res) => { ... });`.+ Inside the route handler, extract the token from the request bod...

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,5 @@
-const passport = require('passport');
+const User = require('../models/User'); // Assuming the User model exists and is at this path
+const PasswordResetLink = require('../models/PasswordResetLink'); // Assuming the PasswordResetLink model exists and is at this path
 
 module.exports = app => {
   app.get(
@@ -23,5 +24,44 @@ module.exports = app => {
 
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
+  });
+
+  // New route handler for email verification
+  app.post('/api/verify_account', async (req, res) => {
+    try {
+      const { token } = req.body;
+      if (!token) {
+        return res.status(400).send({ error: 'Token is required' });
+      }
+
+      const passwordResetLink = await PasswordResetLink.findOne({ token });
+      if (!passwordResetLink) {
+        return res.status(404).send({ error: 'Token not found' });
+      }
+
+      if (passwordResetLink.used) {
+        return res.status(400).send({ error: 'Token has already been used' });
+      }
+
+      if (passwordResetLink.expires_at < new Date()) {
+        return res.status(400).send({ error: 'Token has expired' });
+      }
+
+      const user = await User.findById(passwordResetLink.user_id);
+      if (!user) {
+        return res.status(404).send({ error: 'User not found' });
+      }
+
+      user.account_verified = true;
+      await user.save();
+
+      passwordResetLink.used = true;
+      await passwordResetLink.save();
+
+      res.send({ message: 'Account has been verified successfully' });
+    } catch (error) {
+      console.error('Error verifying account:', error);
+      res.status(500).send({ error: 'Internal server error' });
+    }
   });
 };
